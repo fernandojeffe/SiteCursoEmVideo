40e9fa21f4abd862a2f20c0a19318fadcad0a373
fernandojeffe <66834802+fernandojeffe@users.noreply.github.com>
2022-10-12 11:03:12 -0300

    12/10

--- pythonProject/mundo 2/loço while/D057 Validação de dados.py
+++ pythonProject/mundo 2/loço while/D057 Validação de dados.py
@@ -1,17 +1,26 @@
-resposta = 'S'
+'''resposta = 'S'
 mascolino = 0
 feminino = 0
 
-while resposta =='S':
-    resposta=str(input('Quer cadastrar qual e o sexo [S/N] ')).upper()
+while resposta == 'S':
+    resposta=str(input('Quer cadastrar qual e o sexo [S/N] ')).strip().upper()[0]
     if resposta == 'S':
-        sexo = str(input('Qual e o sexo [M/F]')).upper()
+        sexo = str(input('Qual e o sexo [M/F]')).strip().upper()[0]
         if sexo == 'M':
             mascolino += 1
         if sexo == 'F':
             feminino += 1
         else:
             print('Opção invalida tente novamente! ')
-print('Foram registrados {} do sexo mascolino!'.format(mascolino))
-print('foram registrados {} do sexo feminino!'.format(feminino))
-print('Fim')
+    elif resposta == 'n':
+        print('Foram registrados {} do sexo mascolino!'.format(mascolino))
+        print('foram registrados {} do sexo feminino!'.format(feminino))
+        print('Fim')
+    else:
+        print('Opção invalida')'''
+
+        # guanabara 
+sexo = str(input('informe seu sexo: [M/F]')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo + str(input('Dados inválidos. Por favor, informe sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
---
+++ pythonProject/mundo 2/loço while/d060 Calculo do fatorial.py
@@ -0,0 +1,6 @@
+
+num = int(input('Digite um número'))
+for c in range (1, num + 1):
+    soma += c
+    mult = soma * c
+    print (mult)
