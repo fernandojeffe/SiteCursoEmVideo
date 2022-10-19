9293a241860e21a2ee034f791b8a10172e3d171d
fernandojeffe <66834802+fernandojeffe@users.noreply.github.com>
2022-10-18 21:51:27 -0300

    18/10

---
+++ pythonProject/mundo 2/loço while/d066 varios números com flag.py
@@ -0,0 +1,9 @@
+contador = soma = num = 0
+while num != 999:
+    num = int(input('digite um número (999 para parar):  '))
+    contador += 1
+    if num == 999:        
+        break
+    soma += num
+
+print(f'A soma dos {contador} e foi de {soma}')
---
+++ pythonProject/mundo 2/loço while/d067 Tabuada v3.0.py
@@ -0,0 +1,7 @@
+from os import initgroups
+cont = num = mult = 0
+
+while num == -num:
+    num = int(input('Quer ver tabuada de qual valor: '))
+    cont += 1
+    mult *= num
