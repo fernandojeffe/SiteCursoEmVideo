cf58455b3b8129033d2ba538be9b960ee14e7bd0
fernandojeffe <66834802+fernandojeffe@users.noreply.github.com>
2023-06-29 01:25:44 -0300

    29/06

--- frontrendMentor/newsletter-sign-up-with-success-message-main/script.js
+++ frontrendMentor/newsletter-sign-up-with-success-message-main/script.js
@@ -1,3 +1,30 @@
-{
+function validoEmail(){
+    const emailInput = document.getElementById("email-input");
+    const emailErroMsn = document.getElementById("emailErro-Msn");
+    const notificaBtn = document.getElementById("notifica-btn");
+    const email = emailInput.value;
+    const emailV = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-}
+
+    if (!emailV.test(email)){
+      emailInput.style.borderColor = "hsl(354, 100%, 66%)";
+      emailInput.style.background = "hsl(5, 100%, 95%)";
+      emailErroMsn.style.display = "block";
+      notificaBtn.disabled = true;
+    } else {
+      emailInput.style.borderColor = "";
+      emailErroMsn.style.display = "none";
+      notificaBtn.disabled = false;
+    }
+  }
+
+  function limpar(){
+    document.getElementById("email-input").valeu = "";
+  }
+
+  function success(){
+    const emailInput =document.getElementById("email-input")
+    const notificaBtn = document.getElementById("notifica-btn");
+    const puxarPgn = 
+  }
+  
