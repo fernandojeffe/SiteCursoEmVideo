d67681198635012d42c4ae7812d8d9483b3b1bbb
fernandojeffe <66834802+fernandojeffe@users.noreply.github.com>
2023-07-29 21:54:58 -0300

    29/07

--- frontrendMentor/space-tourism-website-main/starter-code/script.js
+++ frontrendMentor/space-tourism-website-main/starter-code/script.js
@@ -1,55 +1,44 @@
-HTML CSS BabelResult Skip Results Iframe
-EDIT ON
-(function() {
+const nav = document.querySelector(".nav");
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".menu");
 
-  const target = document.querySelector(".target");
-  const links = document.querySelectorAll(".menu a");
-  const colors = ["deepskyblue", "orange", "firebrick", "gold", "magenta", "black", "darkblue"];
+function handleButtonClick(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  event.stopPropagation();
+  nav.classList.toggle("active");
+  handleClickOutside(menu, () => {
+    nav.classList.remove("active");
+    setAria();
+  });
+  setAria();
+}
 
-  function mouseenterFunc() {
-    if (!this.parentNode.classList.contains("active")) {
-      for (let i = 0; i < links.length; i++) {
-        if (links[i].parentNode.classList.contains("active")) {
-          links[i].parentNode.classList.remove("active");
-        }
-        links[i].style.opacity = "0.25";
-      }
-
-      this.parentNode.classList.add("active");
-      this.style.opacity = "1";
-
-      const width = this.getBoundingClientRect().width;
-      const height = this.getBoundingClientRect().height;
-      const left = this.getBoundingClientRect().left + window.pageXOffset;
-      const top = this.getBoundingClientRect().top + window.pageYOffset;
-      const color = colors[Math.floor(Math.random() * colors.length)];
-
-      target.style.width = `${width}px`;
-      target.style.height = `${height}px`;
-      target.style.left = `${left}px`;
-      target.style.top = `${top}px`;
-      target.style.borderColor = color;
-      target.style.transform = "none";
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement;
+  function handleHTMLClick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute("data-target");
+      html.removeEventListener("click", handleHTMLClick);
+      html.removeEventListener("touchstart", handleHTMLClick);
+      callback();
     }
   }
-
-  for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", (e) => e.preventDefault());
-    links[i].addEventListener("mouseenter", mouseenterFunc);
+  if (!targetElement.hasAttribute("data-target")) {
+    html.addEventListener("click", handleHTMLClick);
+    html.addEventListener("touchstart", handleHTMLClick);
+    targetElement.setAttribute("data-target", "");
   }
+}
 
-  function resizeFunc() {
-    const active = document.querySelector(".mynav li.active");
-
-    if (active) {
-      const left = active.getBoundingClientRect().left + window.pageXOffset;
-      const top = active.getBoundingClientRect().top + window.pageYOffset;
-
-      target.style.left = `${left}px`;
-      target.style.top = `${top}px`;
-    }
+function setAria() {
+  const isActive = nav.classList.contains("active");
+  btnMenu.setAttribute("aria-expanded", isActive);
+  if (isActive) {
+    btnMenu.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    btnMenu.setAttribute("aria-label", "Abrir Menu");
   }
+}
 
-  window.addEventListener("resize", resizeFunc);
-
-})();
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
